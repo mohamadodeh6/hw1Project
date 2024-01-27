10c729208cbc72e9bb885c50ad5f85a2edb3ad23
mohmddra <drawshe772@gmail.com>
2024-01-27 18:41:31 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    //hello my friend this is a test for github
+    //hello my friend this is a test for gitHub
     public static Scanner scanner;  // Note: Do not change this line.
 //*************//////////
     /**
@@ -12,7 +12,7 @@ public class Main {
      * @param args (String[]) args[0] is the path to the input file
      */
     public static void main(String[] args) throws IOException {
-        String path = args[0];
+        String path = args[ 0];
         scanner = new Scanner(new File(path));
         int numberOfGames = scanner.nextInt();
         scanner.nextLine();
@@ -20,52 +20,71 @@ public class Main {
         for (int i = 1; i <= numberOfGames; i++) {
             System.out.println("Game number " + i + " starts.");
             theStudentsGame();
+            System.out.println("dddddddddddddddddddddddddddddddddddddddddddddddddddddd");
             System.out.println("Game number " + i + " ended.");
             System.out.println("-----------------------------------------------");
         }
         System.out.println("All games have ended.");
     }
 
+    public static int countX(char[][] board, int rows, int columns){
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (board[i][j] == 'X'){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
     /**
      * Add your description
      */
     public static void theStudentsGame() {
         int[][][] semestersBox;
         char[][] prevBoard, currentBoard;
-        int semesterCount = 0, prevTakeen, currentTakeen = 0;
+        int semesterCount = 0, prevTakeen, currentTakeen = 1, maxSemesters = 100;
         System.out.println("Dear president, please enter the board’s size.");
-        int rows = scanner.nextInt();
-        int cols = scanner.nextInt();
+        String input = scanner.nextLine();
+        String[] dimensions = input.split(" X ");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
         semestersBox = new int[rows][cols][];
         prevBoard = new char[rows][cols];
+        initBoard(prevBoard, rows, cols);
         currentBoard = new char[rows][cols];
-        System.out.println("Dear president, please enter the cell’s indexes.");
         prevTakeen = initBoardIndexes(prevBoard, rows, cols);
-        while(prevTakeen != currentTakeen && currentTakeen != 0){
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    if (Takeen(prevBoard, rows, cols, i, j)){
-                        currentBoard[i][j] = 'X';
-                    }else{
-                        currentBoard[i][j] = '-';
-                    }
-                }
+        for (int i = 1; i <= maxSemesters; i++) {
+            if (prevTakeen == currentTakeen || currentTakeen == 0){
+                break;
             }
-
+            System.out.println("Semester number " + i + ":");
+            printBoard(prevBoard, rows, cols);
+            updateBoard(prevBoard, currentBoard, rows, cols);
+            prevTakeen = countX(prevBoard, rows, cols);
+            currentTakeen = countX(currentBoard, rows, cols);
+            switchBoard(prevBoard, currentBoard, rows,cols);
+            System.out.println("Number of students: " + currentTakeen);
+            System.out.println();
         }
-
-
+    }
+    public static void initBoard(char[][] board, int rows, int cols){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = '-';
+            }
+        }
     }
     //**********//
     public static int initBoardIndexes(char[][] board, int row_max, int col_max){
         int rows = 0, cols = 0, i = 0;
         while(true){
-            if(scanner.hasNextInt()){
-                rows = scanner.nextInt();
-            }
-            if(scanner.hasNextInt()){
-                cols = scanner.nextInt();
-            }
+            System.out.println("Dear president, please enter the cell’s indexes.");
+            String input = scanner.nextLine();
+            String[] dimensions = input.split(", ");
+             rows = Integer.parseInt(dimensions[0]);
+             cols = Integer.parseInt(dimensions[1]);
 
             if(rows < 0 || rows >= row_max || cols < 0 || cols >= col_max){
                 System.out.print("The cell is not within the board’s boundaries, enter a new cell.");
@@ -85,22 +104,12 @@ public class Main {
         return i;
         //*******//
     }
-    public static int countX(int[][] board, int rows, int columns){
-        int count = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (board[i][j] == 'X'){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
     public static void printBoard(char[][] board, int rows, int columns){
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                System.out.println(board[i][j]);
+                System.out.print(board[i][j]);
             }
+            System.out.println();
         }
     }
     public static void updateBoard(char[][] prevBoard, char[][] currentBoard, int rows, int columns){
@@ -138,5 +147,12 @@ public class Main {
             takeen = takeenFriends == 3;
         }
         return takeen;
+    }
+    public static void switchBoard(char[][] prevBoard, char[][] currentBoard, int rows, int columns){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                prevBoard[i][j] = currentBoard[i][j];
+            }
+        }
     }
 }
